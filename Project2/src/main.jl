7e5bfdfd3b14ba5dd791e6a8375b48dc3a961aea
olavforland <olavfo@stud.ntnu.no>
2024-02-26 01:01:23 +0100

    Created time and capacity unfitness.

--- Project2/src/main.jl
+++ Project2/src/main.jl
@@ -2,11 +2,10 @@
 
 include("DataParser.jl")
 include("Genetics.jl")
-include("Modules.jl")
 include("Utils.jl")
 
 using .DataParser: parse_data
-using .Genetics: Chromosome, compute_fitness!
+using .Genetics: Chromosome, compute_fitness!, compute_unfitness!
 # push!(LOAD_PATH, pwd())
 
 
@@ -16,6 +15,15 @@ filepath = joinpath("data", "train_0.json")
 # Parse the data from the file
 instance = parse_data(filepath)
 
+test = Chromosome([82, 83, 86, 87, 90, 84, 89, 85, 88, 91])
+test.phenotype = [[82, 83, 86, 87, 90, 84, 89, 85, 88, 91]]
+
+compute_fitness!(test, instance)
+compute_unfitness!(test, instance)
+
+println(test.fitness)
+println(test.time_unfitness)
+println(test.strain_unfitness)
 
 
 

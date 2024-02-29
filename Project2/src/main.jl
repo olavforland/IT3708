e7b0a0aae2f62dfe921c9eca5d95d29be703558b
jakobke <jakobke@stud.ntnu.no>
2024-02-29 16:24:23 +0100

    Savelsbergh heuristic should work at this point. One todo in function while loop

--- Project2/src/main.jl
+++ Project2/src/main.jl
@@ -13,6 +13,7 @@ include("LambdaInterchange.jl")
 include("GA.jl")
 
 
+
 using .DataParser: parse_data, Patient
 using .Genetics: Chromosome, compute_fitness!, compute_unfitness!
 using .GA: initialize_population, genetic_algorithm
@@ -22,6 +23,7 @@ using .Crossover: two_point_crossover
 using .Selection: tournament_selection, survivor_selection!
 using .LambdaInterchange: lambda_shift_operation
 # push!(LOAD_PATH, pwd())
+using .TSPHeuristic: savelsbergh_heuristic
 
 instance_nr = 0
 
@@ -32,7 +34,7 @@ writepath = joinpath("solutions", "train_" * string(instance_nr) * ".json")
 # Parse the data from the file
 instance = parse_data(readpath)
 
-population = genetic_algorithm(instance, 30, 200000, 0.3, instance.n_nurses)
+population = genetic_algorithm(instance, 30, 10000, 0.3, instance.n_nurses)
 
 best_individual = sort(population, by=p -> (p.time_unfitness, p.strain_unfitness, p.fitness))[1]
 
@@ -43,4 +45,14 @@ write_chromosome_to_file(best_individual, writepath)
 
 
 
+#Just testing heuristic.
+"""
 
+best_individual.phenotype = [Vector{Int}() for _ in 1:instance.n_nurses]
+
+#for each nurse, generate a solution using the savelsbergh heuristic
+for nurse in unique(best_individual.genotype)
+    best_individual.phenotype[nurse] = savelsbergh_heuristic(instance, best_individual, nurse)
+end
+
+"""

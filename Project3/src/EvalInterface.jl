fc841c2bd0b753b9dc479a4a71381c5217f65c8f
jakobke <jakobke@stud.ntnu.no>
2024-04-17 16:51:37 +0200

    made eval-interface

---
+++ Project3/src/EvalInterface.jl
@@ -0,0 +1,41 @@
+module EvalInterface
+
+
+using ..Genetics: Chromosome, get_segment_mask
+using ..Problem: ProblemInstance
+
+
+function draw_segments(chromosome::Chromosome, instance::ProblemInstance, path::String="output.txt")
+    mask = get_segment_mask(chromosome, instance)
+    h = instance.height
+    w = instance.width
+    img = [[255 for _ in 1:w] for _ in 1:h]
+
+    for i in 1:h-1
+        for j in 1:w-1
+            if (mask[i][j] != mask[i+1][j]) || (mask[i][j] != mask[i][j+1])
+                img[i][j] = 0
+            end #if
+        end #for
+    end #for
+
+    #Draw edges of image
+    for i in 1:h
+        img[i][1] = 0
+        img[i][w] = 0
+    end #for
+    for j in 1:w
+        img[1][j] = 0
+        img[h][j] = 0
+    end #for
+
+    #write vector to txt file
+    io = open(path, "w")
+    for line in img
+        println(io, join(line, ","))
+    end #for
+    close(io)
+end #function
+
+
+end #module
--- Project3/src/main.jl
+++ Project3/src/main.jl
@@ -6,12 +6,14 @@ include("Problem.jl")
 include("Genetics.jl")
 include("MOEA.jl")
 include("GeneticOperators.jl")
+include("EvalInterface.jl")
 
 
 using .MOEA: multi_obj_GA, initialize_population
 using .Genetics: Chromosome
 using .Utils: read_image
 using .Problem: ProblemInstance
+using .EvalInterface: draw_segments
 
 function main()
     print("Main function\n")
@@ -25,6 +27,9 @@ function main()
     for i in 1:10
         println(population[i].edge, " ", population[i].connectivity, " ", population[i].deviation)
     end #for
+
+    draw_segments(population[1], probleminstance)
+
     # args: population, p_cross, p_mut, n_generations, n_offspring
     multi_obj_GA(population, 0.8, 0.1, 100, 10)
 
